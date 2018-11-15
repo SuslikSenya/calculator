0e19a92fba43a9fa50770e2883b171f12bdd8735
Sergii Piatakov <sergii.piatakov@globallogic.com>
2018-11-15 15:43:25 +0200

    Add sum operation

--- calculator.cpp
+++ calculator.cpp
@@ -9,3 +9,23 @@ int Calculator::Sub (double a, double b)
 {
     return Add (a, -b);
 }
+
+int Calculator::Mul (double a, double b)
+{
+    return a * b + 0.5;
+}
+
+int main() {
+    int a = 5;
+    int b = 10;
+    int result = a * b;
+    std::cout << "Multiplication result: " << result << std::endl;
+
+    // Добавленные изменения
+    int c = 7;
+    int sum = a + b + c;
+    std::cout << "Sum result: " << sum << std::endl;
+
+    return 0;
+}
+
